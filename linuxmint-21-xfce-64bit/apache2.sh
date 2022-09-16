4000e72b81bfc25e038faa65a18954dde13ba6e4
Vasilache Ionut <vasilacheorionut@gmail.com>
2022-09-16 22:22:46 +0200

    first commit

--- linuxmint-21-xfce-64bit/apache2.sh
+++ linuxmint-21-xfce-64bit/apache2.sh
@@ -1,3 +1,4 @@
 sudo systemctl status apache2
 sudo apt install apache2
+echo "apache2 -version"
 apache2 -version
--- linuxmint-21-xfce-64bit/linuxmint-21-xfce-64bit.sh
+++ linuxmint-21-xfce-64bit/linuxmint-21-xfce-64bit.sh
@@ -3,5 +3,4 @@ bash update.sh
 bash upgrade.sh
 bash apache2.sh
 bash ufw.sh
-sudo systemctl status apache2
 hostname -I
