c5b604c34f8c26eb880051021eef7656d42a9cd3
Vasilache Ionut <vasilacheorionut@gmail.com>
2022-09-16 22:42:45 +0200

    first commit

--- linuxmint-21-xfce-64bit/mysql.sh
+++ linuxmint-21-xfce-64bit/mysql.sh
@@ -1,13 +1,14 @@
 echo 'Install MySQL'
 sudo apt install mysql-server 
-sudo mysql_secure_installation
+echo "
+    SELECT user,authentication_string,plugin,host FROM mysql.user;
+
+    ALTER USER 'root'@'localhost' 
+    IDENTIFIED WITH mysql_native_password 
+    BY 'Test54321@';
+
+    SELECT user,authentication_string,plugin,host FROM mysql.user;
+    mysql -u root -p
+"
 sudo mysql
 
-SELECT user,authentication_string,plugin,host FROM mysql.user;
-
-ALTER USER 'root'@'localhost' 
-IDENTIFIED WITH mysql_native_password 
-BY 'Test54321@';
-
-SELECT user,authentication_string,plugin,host FROM mysql.user;
-mysql -u root -p
