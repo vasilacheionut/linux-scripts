309a3daffb6917132e3faf03597217c7f5f4bd24
Vasilache Ionut <57985027+vasilacheionut@users.noreply.github.com>
2022-09-19 21:04:53 +0200

    Update

--- php.sh
+++ php.sh
@@ -1,6 +1,20 @@
+echo "Update System"
+sudo apt-get update
+
+echo "Install PHP"
 sudo apt install php libapache2-mod-php php-mysql
+
+echo "Configure dir.conf and move index.php after DirectoryIndex"
 sudo nano /etc/apache2/mods-enabled/dir.conf
+
+echo "Restart Apache Server"
 sudo systemctl restart apache2
+
+echo "Verify Apache system service status"
 sudo systemctl status apache2
-echo "phpinfo();"
+
+echo "Create test file info.php and add code above"
 sudo nano /var/www/html/info.php
+echo "<?php phpinfo(); ?>"
+
+echo "Open: localhost/info.php"
