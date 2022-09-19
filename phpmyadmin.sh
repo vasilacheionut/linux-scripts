032d4151c871319bef67dd4ab4ead0400ff79f4d
Vasilache Ionut <57985027+vasilacheionut@users.noreply.github.com>
2022-09-19 22:06:13 +0200

    Update

---
+++ phpmyadmin.sh
@@ -0,0 +1,48 @@
+sudo apt install apache2 wget unzip 
+sudo apt install php php-zip php-json php-mbstring php-mysql 
+
+sudo systemctl enable apache2 
+sudo systemctl start apache2 
+
+
+wget https://files.phpmyadmin.net/phpMyAdmin/5.2.0/phpMyAdmin-5.2.0-all-languages.zip 
+unzip phpMyAdmin-5.2.0-all-languages.zip 
+sudo mv phpMyAdmin-5.2.0-all-languages /usr/share/phpmyadmin 
+
+sudo mkdir /usr/share/phpmyadmin/tmp 
+sudo chown -R www-data:www-data /usr/share/phpmyadmin 
+sudo chmod 777 /usr/share/phpmyadmin/tmp
+
+sudo nano /etc/apache2/conf-available/phpmyadmin.conf 
+
+
+Alias /phpmyadmin /usr/share/phpmyadmin
+Alias /phpMyAdmin /usr/share/phpmyadmin
+ 
+<Directory /usr/share/phpmyadmin/>
+   AddDefaultCharset UTF-8
+   <IfModule mod_authz_core.c>
+      <RequireAny>
+      Require all granted
+     </RequireAny>
+   </IfModule>
+</Directory>
+ 
+<Directory /usr/share/phpmyadmin/setup/>
+   <IfModule mod_authz_core.c>
+     <RequireAny>
+       Require all granted
+     </RequireAny>
+   </IfModule>
+</Directory>
+
+
+sudo a2enconf phpmyadmin 
+sudo systemctl restart apache2 
+
+sudo firewall-cmd --permanent --add-service=http 
+sudo firewall-cmd --reload 
+
+http://your-server-ip-domain/phpmyadmin
+
+http://localhost/phpmyadmin
