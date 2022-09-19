a6bccc94c2596bc817697fd8b2388239ae01b09f
Vasilache Ionut <57985027+vasilacheionut@users.noreply.github.com>
2022-09-19 21:33:28 +0200

    Update

--- ssh.sh
+++ ssh.sh
@@ -9,3 +9,7 @@ sudo apt install openssh-client
 
 echo "Or if whant to be server Install openssh-server that include and openssh-client"
 sudo apt install openssh-client
+
+
+echo "Connect to server"
+ssh learnlinux@192.168.1.144
