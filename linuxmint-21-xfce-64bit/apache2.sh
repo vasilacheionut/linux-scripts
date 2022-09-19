9cf36d8529b8e666607c9aa305abd3111fbe8ff4
Vasilache Ionut <57985027+vasilacheionut@users.noreply.github.com>
2022-09-19 19:53:03 +0200

    Update apache2.sh

--- linuxmint-21-xfce-64bit/apache2.sh
+++ linuxmint-21-xfce-64bit/apache2.sh
@@ -1,3 +1,23 @@
+echo "Install Apache Web Server"
+
+echo "Update System"
+sudo apt-get update
+
+echo "Verify Apache system service status"
+sudo systemctl status apache2
+
+echo "If Apache system service not exists install it"
 sudo apt install apache2
-echo "apache2 -version"
+
+echo "Check Apache Version"
 apache2 -version
+
+echo "Configure the Firewall Settings"
+sudo ufw app list
+sudo ufw allow 'Apache Full'
+
+echo "Verify Apache system service status"
+sudo systemctl status apache2
+
+echo "Verify Hostname IP"
+hostname -I
